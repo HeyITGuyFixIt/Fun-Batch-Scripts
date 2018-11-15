695a46c2cf4ba60d7141e6253338dde5dbfe2f2a
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2018-11-15 09:15:39 -0500

    Update get help.bat

--- get help.bat
+++ get help.bat
@@ -2,7 +2,7 @@
 TITLE Get Help File
 ECHO:Type a command to get the help file.
 SET /P getHelp="Command: "
-%getHelp% /? >>"C:\Users\%USERNAME%\Desktop\%getHelp%Help.txt"
-ECHO:Go to your Desktop folder and look for "%getHelp%Help.txt".
+%getHelp% /? >>"C:\Users\%USERNAME%\Documents\%getHelp%Help.txt"
+ECHO:Go to your Documents folder and look for "%getHelp%Help.txt".
 PAUSE
 EXIT
